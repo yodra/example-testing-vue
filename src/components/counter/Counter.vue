a9227047762bd5025c7ed679ffa3560113cb8deb
Yodra López <yodralopez@gmail.com>
2020-12-01 17:55:23 +0000

    add Counter component with test in Vue Test Utils

---
+++ src/components/counter/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="counter">You clicked {{ count }} times</p>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({count: 0}),
+  methods: {
+    increment() {
+      this.count++;
+    }
+  }
+};
+</script>
